759db61aa4038d4e2c1d726799f8df93c30972db
Carl Z <dev@carlzhou.com>
2023-07-28 22:39:29 -0400

    fix mutation issue

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,15 +1,17 @@
 import { HOST_CONFIG_MAP, ISiteConfig } from "./config";
 
+const waitForLoaded = (selector: string, callback: () => void) => {
+    if (document.querySelectorAll(selector).length) {
+      callback();
+    } else {
+      setTimeout(() => {
+            waitForLoaded(selector, callback);
+        }, 100);
+    }
+};
+
+
 const run = (config: ISiteConfig) => {
-    const waitForLoaded = (selector: string, callback: () => void) => {
-        if (document.querySelectorAll(selector).length) {
-          callback();
-        } else {
-          setTimeout(() => {
-                waitForLoaded(selector, callback);
-            }, 100);
-        }
-    };
     
     const makeOddsTextSelectable = (): void => {
     
@@ -27,13 +29,16 @@ const run = (config: ISiteConfig) => {
 const hostConfig = HOST_CONFIG_MAP[location.host];
 run(hostConfig);
 
-const targetNode = document.querySelector(hostConfig.mutationObserver);
-const mutationObserverConfig = { attributes: true, childList: true, subtree: true };
-const observer = new MutationObserver((mutationList, _observer) => {
-    if (mutationList.length > 0) {
-        run(hostConfig);
+waitForLoaded(hostConfig.wait, () => {
+    const targetNode = document.querySelector(hostConfig.mutationObserver);
+    const mutationObserverConfig = { attributes: true, childList: true, subtree: true, characterData: true };
+    const observer = new MutationObserver((mutationList, _observer) => {
+        if (mutationList.length > 0) {
+            run(hostConfig);
+        }
+    });
+    if (targetNode) {
+        observer.observe(targetNode, mutationObserverConfig);
     }
-  });
-if (targetNode) {
-    observer.observe(targetNode, mutationObserverConfig);
-}
+});
+
